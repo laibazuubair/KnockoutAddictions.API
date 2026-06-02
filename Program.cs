a69c72ddc11518ac17dc29a899354aad75d1e999
laibazuubair <laibazuubair@gmail.com>
2026-06-02 21:33:25 +0100

    fix railway 500 error + auto migrate db

--- Program.cs
+++ Program.cs
@@ -109,4 +109,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    // This creates tables automatically in Railway
+    db.Database.Migrate();
+}
 app.Run();
